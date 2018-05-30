a43cb5e673d8940ef661611435d4cb32f78b526a
Yusuf Elasoshi Shamsudeen <shamoyusuf@gmail.com>
2018-05-30 11:59:36 +0530

    update

--- src/App.js
+++ src/App.js
@@ -6,13 +6,7 @@ import LoginForm from './components/LoginForm';
 
 class App extends Component {
   componentWillMount() {
-    firebase.initializeApp({
-    apiKey: 'AIzaSyB0HTh_7j0bLm6KIslduXsJhpWsPfIgDrs',
-    authDomain: 'auth-8b666.firebaseapp.com',
-    databaseURL: 'https://auth-8b666.firebaseio.com',
-    projectId: 'auth-8b666',
-    storageBucket: 'auth-8b666.appspot.com',
-    messagingSenderId: '1061421106474'
+    Enter your firebase api
   });
   }
 
